72e0ef973987104da6e76e39e06f2c62a899e46f
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-05 23:43:26 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const cells = document.querySelectorAll('[data-cell]');
+const messageDiv = document.getElementById('message');
+const restartButton = document.getElementById('restart');
+
+let currentPlayer = 'X';
+let board = Array(9).fill(null);
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+cells.forEach((cell, index) => {
+    // Add eventListener for click.
+    cell.addEventListener('click', () => {
+        // Check that user try to clikc on same button or the user is already win.
+        if(cell.textContent || checkWinner()) return;
+
+        // Update text in the cell and also update the value in board array.
+        cell.textContent = currentPlayer;
+        board[index] = currentPlayer;
+
+        // Check for the winner
+        if(checkWinner()) {
+            messageDiv.textContent = `Player ${currentPlayer} Wins!`;
+            return;
+        }
+
+        if(board.every(cell => cell)) {
+            messageDiv.textContent = "It's a Draw!";
+            return;
+        }
+
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        messageDiv.textContent = `Player ${currentPlayer}'s Turn`;
+    });
+
+});
+
