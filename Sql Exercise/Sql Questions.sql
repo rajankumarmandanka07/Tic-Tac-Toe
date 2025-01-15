d2cb480322dfb403f76486aa9a6e7c4ddd5af6de
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-15 17:17:56 +0530

    Display the first and last name of actor from actor table

--- Sql Exercise/Sql Questions.sql
+++ Sql Exercise/Sql Questions.sql
@@ -8,3 +8,6 @@ DESCRIBE actor;
 
 -- Q3 Display the schema which was used to create table “actor” and view the complete schema using the viewer. (1 row) 
 SHOW CREATE TABLE actor;
+
+-- Q4 Display the first and last names of all actors from the table actor. (200 rows) 
+SELECT first_name, last_name FROM actor;
