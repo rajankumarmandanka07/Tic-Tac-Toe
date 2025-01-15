aa0b86d26529d7fde022f478512fc946718b5ac2
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-15 17:20:23 +0530

    Display the actores Id, first and last name who has first name Joe

--- Sql Exercise/Sql Questions.sql
+++ Sql Exercise/Sql Questions.sql
@@ -16,3 +16,8 @@ SELECT first_name, last_name FROM actor;
 SELECT first_name, last_name 
 FROM actor 
 WHERE last_name = 'Johansson';
+
+-- Q7 You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information? (1 row) 
+SELECT actor_id, first_name, last_name 
+FROM actor 
+WHERE first_name = 'Joe';
