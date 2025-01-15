26cff6a0e69217dc5929208b6f132b4cc63688df
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-15 17:16:22 +0530

    Display the schema of actore table

--- Sql Exercise/Sql Questions.sql
+++ Sql Exercise/Sql Questions.sql
@@ -5,3 +5,6 @@ SHOW TABLES;
 
 -- Q2 Display structure of table “actor”. (4 row)
 DESCRIBE actor;
+
+-- Q3 Display the schema which was used to create table “actor” and view the complete schema using the viewer. (1 row) 
+SHOW CREATE TABLE actor;
