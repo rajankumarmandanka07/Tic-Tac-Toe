1f1524f1afdf32bd36d9f80dda2e81b709f3c89f
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-17 10:55:35 +0530

    Display the name and length of track.

--- Sqlite Exercise/Chinook_Sqlite.sql
+++ Sqlite Exercise/Chinook_Sqlite.sql
@@ -2,3 +2,8 @@
 SELECT AlbumId, Title
 FROM Album
 WHERE AlbumId = 67;
+
+-- Q.2 Find the name and length (in seconds) of all tracks that have length between 50 and 70 seconds. 
+SELECT Name, Milliseconds / 1000
+FROM Track
+WHERE Milliseconds / 1000 BETWEEN 50 AND 70;
