3b451afd997a47c252752711e07cfed3aeef1116
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-17 11:24:41 +0530

    List all the albums by artists with the word ‘black’ in their name

--- Sqlite Exercise/Chinook_Sqlite.sql
+++ Sqlite Exercise/Chinook_Sqlite.sql
@@ -7,3 +7,9 @@ WHERE AlbumId = 67;
 SELECT Name, Milliseconds / 1000
 FROM Track
 WHERE Milliseconds / 1000 BETWEEN 50 AND 70;
+
+-- Q.3 List all the albums by artists with the word ‘black’ in their name. 
+SELECT Al.Title, Ar.Name
+FROM Album Al
+JOIN Artist Ar ON Al.ArtistId = Ar.ArtistId
+WHERE Ar.Name LIKE '%black%';
