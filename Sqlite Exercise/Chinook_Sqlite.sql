c6e277b0c58a7c23a1d9a1db8c338976dd3741d7
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-19 13:40:31 +0530

    Provide a query showing the Invoices of customers who are from Brazil

--- Sqlite Exercise/Chinook_Sqlite.sql
+++ Sqlite Exercise/Chinook_Sqlite.sql
@@ -61,3 +61,12 @@ FROM MediaType m
 JOIN Track t ON m.MediaTypeId = t.MediaTypeId
 GROUP BY m.MediaTypeId
 ORDER BY COUNT(TrackId) DESC;
+
+-- Q.2 Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country. 
+SELECT c.FirstName || ' ' || c.LastName AS Customer_Name, 
+       i.InvoiceId, 
+       i.InvoiceDate, 
+       i.BillingCountry
+FROM Invoice i
+JOIN Customer c ON i.CustomerId = c.CustomerId
+WHERE c.Country = 'Brazil';
