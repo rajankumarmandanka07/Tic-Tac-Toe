b3fde69fc88ae7448810c11769175fe72b2db3bf
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-01-21 11:54:34 +0530

    Write a query to display the order_id, customer id and customer full name of customers along with (product_quantity) as total quantity of products shipped for order ids > 10060

--- Order_Management/Order_Management_Exercise.sql
+++ Order_Management/Order_Management_Exercise.sql
@@ -3,7 +3,8 @@ USE orders;
 -- 1. Write a query to Display the product details (product_class_code, product_id, product_desc, product_price,) as per the following criteria and sort them in descending order of category: 
 -- a. If the category is 2050, increase the price by 2000 
 -- b. If the category is 2051, increase the price by 500 
--- c. If the category is 2052, increase the price by 600. Hint: Use case statement. no permanent change in table required. (60 ROWS) [NOTE: PRODUCT TABLE]
+-- c. If the category is 2052, increase the price by 600. 
+-- Hint: Use case statement. no permanent change in table required. (60 ROWS) [NOTE: PRODUCT TABLE]
 SELECT PRODUCT_CLASS_CODE, PRODUCT_ID, PRODUCT_DESC,
 CASE
 	WHEN PRODUCT_CLASS_CODE = 2050 THEN PRODUCT_PRICE + 2000
@@ -17,7 +18,8 @@ ORDER BY PRODUCT_CLASS_CODE DESC;
 -- 2. Write a query to display (product_class_desc, product_id, product_desc, product_quantity_avail ) and Show inventory status of products as below as per their available quantity: 
 -- a. For Electronics and Computer categories, if available quantity is <= 10, show 'Low stock', 11 <= qty <= 30, show 'In stock', >= 31, show 'Enough stock' 
 -- b. For Stationery and Clothes categories, if qty <= 20, show 'Low stock', 21 <= qty <= 80, show 'In stock', >= 81, show 'Enough stock' 
--- c. Rest of the categories, if qty <= 15 – 'Low Stock', 16 <= qty <= 50 – 'In Stock', >= 51 – 'Enough stock' For all categories, if available quantity is 0, show 'Out of stock'. Hint: Use case statement. (60 ROWS) [NOTE: TABLES TO BE USED – product, product_class]
+-- c. Rest of the categories, if qty <= 15 – 'Low Stock', 16 <= qty <= 50 – 'In Stock', >= 51 – 'Enough stock' For all categories, if available quantity is 0, show 'Out of stock'. 
+-- Hint: Use case statement. (60 ROWS) [NOTE: TABLES TO BE USED – product, product_class]
 
 SELECT pc.product_class_desc, p.product_id, p.product_desc, p.product_quantity_avail,
 CASE 
@@ -44,7 +46,6 @@ END AS inventory_status
 FROM product p
 JOIN product_class pc
 ON p.product_class_code = pc.product_class_code;
--- ORDER BY pc.product_class_desc;
 
 
 -- 3. Write a query to Show the count of cities in all countries other than USA & MALAYSIA, with more than 1 city, in the descending order of CITIES. (2 rows) [NOTE: ADDRESS TABLE, Do not use Distinct]
@@ -59,18 +60,23 @@ ORDER BY city_count DESC;
 SELECT p.PRODUCT_ID, p.PRODUCT_DESC, SUM(oi.PRODUCT_QUANTITY) AS TOTAL_QUANTITY
 FROM ORDER_ITEMS oi
 JOIN PRODUCT p ON oi.PRODUCT_ID = p.PRODUCT_ID
-WHERE p.PRODUCT_ID IN (
+WHERE p.PRODUCT_ID = 201
+   OR (p.PRODUCT_ID IN (
         SELECT PRODUCT_ID
         FROM ORDER_ITEMS
         GROUP BY PRODUCT_ID
-        HAVING SUM(PRODUCT_QUANTITY) = (SELECT MAX(TOTAL_QUANTITY)
-                FROM (SELECT PRODUCT_ID,SUM(PRODUCT_QUANTITY) AS TOTAL_QUANTITY
-                     FROM ORDER_ITEMS
-                     GROUP BY PRODUCT_ID ) AS SubQuery
-				)
-    ) OR p.PRODUCT_ID = 201
-GROUP BY p.PRODUCT_ID, p.PRODUCT_DESC
-ORDER BY TOTAL_QUANTITY DESC;
+        HAVING SUM(PRODUCT_QUANTITY) = (
+            SELECT MAX(TOTAL_QUANTITY)
+            FROM (
+                SELECT PRODUCT_ID, SUM(PRODUCT_QUANTITY) AS TOTAL_QUANTITY
+                FROM ORDER_ITEMS
+                GROUP BY PRODUCT_ID
+            ) AS SubQuery
+        )
+    ) AND p.PRODUCT_ID != 201)
+GROUP BY p.PRODUCT_ID
+ORDER BY CASE WHEN p.PRODUCT_ID = 201 THEN 1 ELSE 2 END
+LIMIT 1;
 
 
 -- 6. Write a query to display the customer_id,customer name, email and order details (order id, product desc,product qty, subtotal(product_quantity * product_price)) for all customers even if they have not ordered any item.(225 ROWS) [NOTE: TABLE TO BE USED - online_customer, order_header, order_items, product]
@@ -95,6 +101,15 @@ ORDER BY CARTON_VOL ASC
 LIMIT 1;
 
 
+-- 9. Write a query to display the order_id, customer id and customer full name of customers along with (product_quantity) as total quantity of products shipped for order ids > 10060. (6 ROWS) [NOTE: TABLES TO BE USED - online_customer, order_header, order_items]
+SELECT oh.ORDER_ID, oc.CUSTOMER_ID, CONCAT(oc.CUSTOMER_FNAME, ' ', oc.CUSTOMER_LNAME) AS CUSTOMER_FULLNAME, SUM(oi.PRODUCT_QUANTITY) AS TOTAL_QUANTITY
+FROM ONLINE_CUSTOMER oc
+JOIN ORDER_HEADER oh ON oc.CUSTOMER_ID = oh.CUSTOMER_ID
+JOIN ORDER_ITEMS oi ON oh.ORDER_ID = oi.ORDER_ID
+WHERE oh.ORDER_ID > 10060
+GROUP BY oh.ORDER_ID
+ORDER BY oh.ORDER_ID;
+
 -- 10. Write a query to display product class description ,total quantity (sum(product_quantity),Total value (product_quantity * product price) and show which class of products have been shipped highest(Quantity) to countries outside India other than USA? Also show the total value of those items. (1 ROWS)[NOTE:PRODUCT TABLE,ADDRESS TABLE,ONLINE_CUSTOMER TABLE,ORDER_HEADER TABLE,ORDER_ITEMS TABLE,PRODUCT_CLASS TABLE]
 
 SELECT pc.PRODUCT_CLASS_DESC, SUM(oi.PRODUCT_QUANTITY) AS TOTAL_QUANTITY, SUM(oi.PRODUCT_QUANTITY * p.PRODUCT_PRICE) AS TOTAL_VALUE
@@ -106,7 +121,6 @@ JOIN ONLINE_CUSTOMER oc ON oh.CUSTOMER_ID = oc.CUSTOMER_ID
 JOIN ADDRESS a ON oc.ADDRESS_ID = a.ADDRESS_ID
 WHERE a.COUNTRY NOT IN ('India', 'USA')
 GROUP BY pc.PRODUCT_CLASS_DESC
-ORDER BY TOTAL_QUANTITY DESC
-LIMIT 1;
+ORDER BY TOTAL_QUANTITY DESC LIMIT 1;
 
     
