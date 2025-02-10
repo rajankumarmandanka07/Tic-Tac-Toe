b860367d3e88f58acf7cd4b84c460c00b77a5329
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-02-10 10:18:25 +0530

    Update UIHandler

---
+++ Battleship/modules/uiHandler.js
@@ -0,0 +1,31 @@
+import { IUIHandler } from "./interfaces.js";
+
+class UIHandler extends IUIHandler {
+    constructor(instructionId, resultsId) {
+        super();
+        this.instructionElement = document.getElementById(instructionId);
+        this.resultsElement = document.getElementById(resultsId);
+    }
+
+    updateInstruction(text) {
+        this.instructionElement.textContent = text;
+    }
+
+    showResults(totalHits, totalMisses) {
+        this.resultsElement.style.display = "block";
+        this.resultsElement.innerHTML = `
+      <h2>Results:</h2>
+      <p>Total Hits: ${totalHits}</p>
+      <p>Total Misses: ${totalMisses}</p>
+      <button id="restart-btn" class="btn btn-success btn-lg mt-3">Restart</button>
+    `;
+    }
+
+    disableBoard() {
+        document.querySelectorAll(".ship").forEach(ship => {
+            ship.style.pointerEvents = "none";
+        });
+    }
+}
+
+export { UIHandler };
