3461b1edcffd8afb96d393719d30275cea12939f
rajankumarmandanka07 <rajankumar.mandanka@mobiuso.com>
2025-02-20 23:21:43 +0530

    Add main.ja

---
+++ Game-of-Life/src/main.js
@@ -0,0 +1,15 @@
+import { GameBoard } from './models/GameBoard.js';
+import { GameNextGeneration } from './models/GameNextGeneration.js';
+import { GameController } from './models/GameController.js';
+
+const rows = 30;
+const cols = 30;
+
+const gameBoard = new GameBoard(rows, cols, 'grid');
+const nextGenLogic = new GameNextGeneration(gameBoard);
+const controller = new GameController(gameBoard, nextGenLogic);
+
+document.getElementById('playBtn').addEventListener('click', () => controller.play());
+document.getElementById('pauseBtn').addEventListener('click', () => controller.pause());
+document.getElementById('resetBtn').addEventListener('click', () => controller.reset());
+document.getElementById('randomBtn').addEventListener('click', () => controller.randomInput());
